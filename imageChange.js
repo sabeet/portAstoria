f43d110e128d6c17cc740f2ad5061ad40b3859d6
sabeet <sabeet.other.way@gmail.com>
2020-06-19 23:57:57 -0400

    javascript working well functionally

--- imageChange.js
+++ imageChange.js
@@ -1,5 +1,22 @@
 function changeImage(){
-    if(doc.getElementById("animation")){
-        document.getElementByClass("img").src == "potatoe.jpg";
+    if(document.getElementById('a')){
+        document.getElementById('special-circle').style.background = 'url(goat.jpg)';
+        
+    }
+
+    else if(document.getElementById('b')){
+        document.getElementById('special-circle').style.background = 'url(goat.jpg)';
+    }
+
+    else if(document.getElementById('c')){
+        document.getElementById('special-circle').style.background = 'url(goat.jpg)';
+    }
+}
+
+//credit to Victor Singh for correcting my mistake here! Thank you!
+
+function returnToOriginal(){
+    if(document.getElementById('a')){
+        document.getElementById('special-circle').style.background = 'url(blackSquir.png)';
     }
 }
