5c9146fdde378471af5f52966df64720943bd59f
sabeet <sabeet.other.way@gmail.com>
2020-06-21 17:14:57 -0400

    Main functionality and image swapping complete for main page

--- imageChange.js
+++ imageChange.js
@@ -1,22 +1,27 @@
 function changeImage(){
     if(document.getElementById('a')){
-        document.getElementById('special-circle').style.background = 'url(goat.jpg)';
-        
-    }
-
-    else if(document.getElementById('b')){
-        document.getElementById('special-circle').style.background = 'url(goat.jpg)';
-    }
-
-    else if(document.getElementById('c')){
-        document.getElementById('special-circle').style.background = 'url(goat.jpg)';
+        document.getElementById('special-circle').style.background = 'url(websiteAnimationMain.png)';
     }
 }
 
-//credit to Victor Singh for correcting my mistake here! Thank you!
+function changeImage2(){
+    if(document.getElementById('b')){
+        document.getElementById('special-circle').style.background = 'url(websiteCodeMain.png)';
+    }
+}
+
+function changeImage3(){
+    if(document.getElementById('c')){
+        document.getElementById('special-circle').style.background = 'url(websiteArtMain.png)';
+    }
+}
+
+//credits to Victor Singh for correcting my mistake here! Thank you!
+//credits to Wadgma Masab for pointing out why the function didn't work! Thank you!
 
 function returnToOriginal(){
-    if(document.getElementById('a')){
-        document.getElementById('special-circle').style.background = 'url(blackSquir.png)';
+    if(document.getElementById('a') || document.getElementById('b') || document.getElementById('c')){
+        document.getElementById('special-circle').style.background = 'url(SACMain.png)';
     }
 }
+
